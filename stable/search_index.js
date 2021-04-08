419a32e6b2fa4f83ccc4019d4364ecbebeacc5ea
Documenter.jl <documenter@juliadocs.github.io>
2021-04-08 20:23:47 +0000

    build based on 69cccbf

---
+++ stable/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#NCutSegmentation.jl","page":"Home","title":"NCutSegmentation.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NCutSegmentation.jl","category":"page"}]
+}
